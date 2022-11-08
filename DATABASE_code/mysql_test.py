61e8ec36d189d51ea42c0d2e5ee78ffcabf4c287
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-11-08 11:27:51 +0900

    Delete mysql_test.py

--- DATABASE_code/mysql_test.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-conn = pymysql.connect(host="127.0.0.1", user="root", password="MySQLhelena5863*", db="soloDB", charset="utf8")
-cur=conn.cursor()
-cur.execute("CREATE  TABLE userTable (id char(4), userName char(15), email char(20), birthYear int)")
-cur.execute("INSERT INTO userTable VALUES( 'hong', '홍지윤', 'hong@naver.com', '1996')")
-cur.execute("INSERT INTO userTable VALUES( 'kim', '김태연', 'kim@naver.com', '2011')")
-
-conn.commit()
-conn.close()
