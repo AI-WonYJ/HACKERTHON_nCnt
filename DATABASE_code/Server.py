d98046603acd39e9bfb7edc455fa6ea9bae9c56f
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-11-08 11:35:11 +0900

    Update Server.py

--- DATABASE_code/Server.py
+++ DATABASE_code/Server.py
@@ -4,7 +4,7 @@ import pymysql
 url = "http://114.71.48.94:8080/nCnt" # url 에 https 가 아닌 http 사용할 것
 
 def sql_recorder(Now_Time, Now_people):
-	conn = pymysql.connect(host="127.0.0.1", user="root", password="MySQLhelena5863*", db="nCntDB", charset="utf8")
+	conn = pymysql.connect(host="127.0.0.1", user="root", password="", db="nCntDB", charset="utf8")
 	cur=conn.cursor()
 	# cur.execute("CREATE  TABLE numberTable (time char(30), people_num INT)")
 	cur.execute("INSERT INTO numberTable VALUES('{0}', '{1}')".format(Now_Time, Now_people))
@@ -12,7 +12,7 @@ def sql_recorder(Now_Time, Now_people):
 	conn.close()
 	
 def sql_maker():
-  conn = pymysql.connect(host="127.0.0.1", user="root", password="MySQLhelena5863*", db="nCntDB", charset="utf8")
+  conn = pymysql.connect(host="127.0.0.1", user="root", password="", db="nCntDB", charset="utf8")
   cur=conn.cursor()
   cur.execute("CREATE  TABLE numberTable (datetime char(30), people_num INT)")
   conn.commit()
